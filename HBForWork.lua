d47015df983547597a81b1c1bbdcdf3c4af35c75
jiutianhuanpei <shenhongbang@163.com>
2020-12-31 13:21:12 +0800

    update sth

--- HBForWork.lua
+++ HBForWork.lua
@@ -1,17 +1,23 @@
 
 local workWifi = "ZYHY-internet"
-local homeWifi = "HUAWEI-RT8AT9"
+local homeWifi1 = "HUAWEI-RT8AT9"
+local homeWifi2 = "mywifi_5G"
+local homeWifi3 = "mywifi_2G"
 
 local outputDeviceName = 'Built-in Output'
+
+local voiceNum = 20
 
 function ssidChangedCallback() -- 回调
     ssid = hs.wifi.currentNetwork() -- 获取当前 WiFi ssid
     if (ssid ~= nil) then
         print("watcher: 当前网络是：" , ssid)
-        if (ssid == homeWifi) then
+        print("aa:", ssid == homeWifi2)
+
+        if (ssid == homeWifi1 or ssid == homeWifi2 or ssid == homeWifi3) then
             hs.notify.new({title="当前位置", informativeText="回家喽"}):send()
             hs.audiodevice.findDeviceByName(outputDeviceName):setOutputMuted(false)
-            hs.audiodevice.findDeviceByName(outputDeviceName):setOutputVolume(65)
+            hs.audiodevice.findDeviceByName(outputDeviceName):setOutputVolume(voiceNum)
         else
             
             hs.notify.new({title="当前位置", informativeText="在公司了"}):send() -- 发出通知
@@ -34,10 +40,10 @@ local currentOutput = hs.audiodevice.current(false)
 
 print("当前网络是：" , currentWifi)
 
-if (currentWifi == homeWifi) then
+if (currentWifi == homeWifi1 or currentWifi == homeWifi2 or ssid == homeWifi3) then
 
     hs.audiodevice.findDeviceByName(outputDeviceName):setOutputMuted(false)
-    hs.audiodevice.findDeviceByName(outputDeviceName):setOutputVolume(65)
+    hs.audiodevice.findDeviceByName(outputDeviceName):setOutputVolume(voiceNum)
     print("在家")
     
 else
